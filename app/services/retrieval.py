bca137dfe9217ccd5aab05f14ebe99587734ecd5
Onur Özcan <onurozcan@etu.edu.tr>
2025-02-11 15:29:22 +0300

    diagno_sys and rag_server connection tested

--- app/services/retrieval.py
+++ app/services/retrieval.py
@@ -1,13 +1,12 @@
 import httpx
 from fastapi import HTTPException
-import os
 
 async def process_query(query: str, top_k: int = 5):
     try:
         async with httpx.AsyncClient() as client:
             response = await client.post(
-                "http://localhost:8001/rag/search",  # RAG container endpoint
-                json={"query": query.question, "top_k": top_k, "collection_name": os.getenv("COLLECTION_NAME"), "hybrid_alpha": 0.5}
+                "http://rag_server:8001/rag/search",  # RAG container endpoint
+                json={"query": query, "top_k": top_k}
             )
         response.raise_for_status()
         return response.json()
